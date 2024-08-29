884fd1f48c190fa254806f0d93d10f452b079c90
nmullaney <nora.mullaney@gmail.com>
2024-08-29 14:53:12 -0700

    Make the UI for TodayScreen slightly nicer

--- app/src/main/java/com/nmullaney/doable/ui/home/TodayScreen.kt
+++ app/src/main/java/com/nmullaney/doable/ui/home/TodayScreen.kt
@@ -1,21 +1,31 @@
 package com.nmullaney.doable.ui.home
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.nmullaney.doable.R
 import com.nmullaney.doable.data.Task
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun TodayScreen(
@@ -28,15 +38,29 @@ fun TodayScreen(
             }
         }
     ) { paddingValues ->
-        Column(modifier = Modifier.padding(paddingValues)) {
-            Text("This is the Today Screen")
+        Column(modifier = Modifier
+            .padding(paddingValues)
+            .fillMaxWidth()
+            .fillMaxWidth()
+            ) {
+            val dateTimeFormatter = DateTimeFormatter.ofPattern("EEE LLL dd, yyyy")
+            val dateString = dateTimeFormatter.format(uiState.value.date)
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .align(CenterHorizontally),
+                text = dateString,
+                textAlign = TextAlign.Center,
+                fontSize = 18.sp)
             TodoSection(uiState.value.todoTasks, onTaskClick = {
                 viewModel.completeTask(it)
             })
-            DoneSection(uiState.value.doneTasks)
+            DoneSection(uiState.value.doneTasks, onTaskClick = {
+                viewModel.undoTask(it)
+            })
         }
     }
-
 }
 
 @Composable
@@ -48,17 +72,40 @@ fun AddTaskButton(onClick: () -> Unit) {
 
 @Composable
 fun TodoSection(todoTasks: List<Task>, onTaskClick: (Task) -> Unit = {}) {
-    Text("TODO")
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.LightGray)
+            .padding(8.dp),
+        text = stringResource(id = R.string.todo),
+        fontSize = 18.sp)
     todoTasks.forEach { task ->
-        Text(modifier = Modifier.clickable { onTaskClick(task) }, text = task.title)
+        TaskCard(task = task, onCheckedChange = { onTaskClick(task) })
     }
 }
 
 @Composable
-fun DoneSection(doneTasks: List<Task>) {
-    Text("Done")
+fun DoneSection(doneTasks: List<Task>, onTaskClick: (Task) -> Unit = {}) {
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.LightGray)
+            .padding(8.dp),
+        text = stringResource(id = R.string.done),
+        fontSize = 18.sp)
     doneTasks.forEach { task ->
+        TaskCard(task = task, onCheckedChange = { onTaskClick(task) })
+    }
+}
+
+@Composable
+fun TaskCard(task: Task, onCheckedChange: (Boolean) -> Unit) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .background(color = Color.White)
+        .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically) {
+        Checkbox(checked = task.isCompleted, onCheckedChange = onCheckedChange)
         Text(task.title)
     }
-
 }
--- app/src/main/java/com/nmullaney/doable/ui/home/TodayViewModel.kt
+++ app/src/main/java/com/nmullaney/doable/ui/home/TodayViewModel.kt
@@ -4,32 +4,41 @@ import androidx.lifecycle.ViewModel
 import com.nmullaney.doable.data.Task
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import java.time.LocalDate
 
 class TodayViewModel: ViewModel() {
     private val todoTasks = mutableListOf<Task>()
     private val doneTasks = mutableListOf<Task>()
+    private val today = LocalDate.now()
 
-    private val _uiState = MutableStateFlow(TodayUiState(todoTasks, doneTasks))
+    private val _uiState = MutableStateFlow(TodayUiState(today, todoTasks, doneTasks))
     val uiState = _uiState.asStateFlow()
 
     fun addTask(task: Task) {
         todoTasks.add(task)
-        _uiState.value = TodayUiState(todoTasks, doneTasks)
+        _uiState.value = TodayUiState(today, todoTasks, doneTasks)
     }
 
     fun removeTask(task: Task) {
         todoTasks.remove(task)
-        _uiState.value = TodayUiState(todoTasks, doneTasks)
+        _uiState.value = TodayUiState(today, todoTasks, doneTasks)
+    }
+
+    fun undoTask(task: Task) {
+        doneTasks.remove(task)
+        todoTasks.add(task.copy(isCompleted = false))
+        _uiState.value = TodayUiState(today, todoTasks, doneTasks)
     }
 
     fun completeTask(task: Task) {
         todoTasks.remove(task)
-        doneTasks.add(task)
-        _uiState.value = TodayUiState(todoTasks, doneTasks)
+        doneTasks.add(task.copy(isCompleted = true))
+        _uiState.value = TodayUiState(today, todoTasks, doneTasks)
     }
 }
 
 class TodayUiState(
+    val date: LocalDate,
     val todoTasks: List<Task>,
     val doneTasks: List<Task>
 )
